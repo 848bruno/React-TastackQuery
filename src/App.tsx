c9e83618a897f9f2d81b65c336778627cf112d76
848bruno <ambalebruno848@gmail.com>
2025-06-23 22:25:54 +0300

    inittial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Suspense } from 'react';
+import Loaders from './combonents/Loaders';
+import User from './combonents/Users';
+
+
+function App() {
+  
+
+  return (
+    <div className="min-h-screen bg-gray-50 place-content-center">
+      <Suspense fallback={<Loaders />}>
+        <User />
+      </Suspense>
+      </div>
+  )
+}
+
+export default App
